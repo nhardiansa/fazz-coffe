d8532e96494ea1a9693a5084c304dd12eeed3972
nhardiansa <nhardiansa@gmail.com>
2024-02-03 16:46:02 +0700

    fix: remove useState

--- frontend/src/features/auth/presenter/components/LoginWrapper.tsx
+++ frontend/src/features/auth/presenter/components/LoginWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import FormLogin, { PropsFormLogin } from './LoginForm'
 import useLoginAdapter from '../../adapters/login'
 
